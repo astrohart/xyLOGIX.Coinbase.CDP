9705c6da6a6b79dad4c0dc2407c0f5bed9d8fb4b
Brian Hart <brian.hart@xylogix.net>
2024-05-20 17:59:34 -0600

    Latest updates at 17:59 hours MDT on 05/20/2024

---
+++ xyLOGIX.Coinbase.CDP.Tokens.Interfaces/IJwtTokenGenerator.cs
@@ -0,0 +1,9 @@
+﻿namespace xyLOGIX.Coinbase.CDP.Tokens.Interfaces
+{
+    /// <summary>
+    /// Defines the publicly-exposed events, methods and properties of an object that
+    /// generates <c>JSON Web Tokens</c> for use on the <c>Coinbase (Retail)</c> or
+    /// <c>Coinbase (Advanced Trade)</c> trading platform(s).
+    /// </summary>
+    public interface IJwtTokenGenerator { }
+}
---
+++ xyLOGIX.Coinbase.CDP.Tokens/JwtTokenGenerator.cs
@@ -0,0 +1,33 @@
+﻿using PostSharp.Patterns.Diagnostics;
+using xyLOGIX.Coinbase.CDP.Tokens.Interfaces;
+
+namespace xyLOGIX.Coinbase.CDP.Tokens
+{
+    /// <summary>
+    /// Generates so-called <c>JSON Web Tokens</c> for use on the
+    /// <c>Coinbase (Retail)</c> or the <c>Coinbase (Advanced Trade)</c> trading
+    /// platform(s).
+    /// </summary>
+    public class JwtTokenGenerator : IJwtTokenGenerator
+    {
+        /// <summary>
+        /// Empty, static constructor to prohibit direct allocation of this class.
+        /// </summary>
+        [Log(AttributeExclude = true)]
+        static JwtTokenGenerator() { }
+
+        /// <summary>
+        /// Empty, protected constructor to prohibit direct allocation of this class.
+        /// </summary>
+        [Log(AttributeExclude = true)]
+        protected JwtTokenGenerator() { }
+
+        /// <summary>
+        /// Gets a reference to the one and only instance of the object that implements the
+        /// <see cref="T:xyLOGIX.Coinbase.CDP.Tokens.Interfaces.IJwtTokenGenerator" />
+        /// interface.
+        /// </summary>
+        public static IJwtTokenGenerator Instance { get; } =
+            new JwtTokenGenerator();
+    }
+}
