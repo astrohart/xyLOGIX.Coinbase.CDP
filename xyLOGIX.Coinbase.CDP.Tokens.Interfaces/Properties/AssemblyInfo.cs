b20438db0815eaf08a8aba503d929605c5ba131d
Brian Hart <brian.hart@xylogix.net>
2024-05-20 15:45:25 -0600

    Add project files.

---
+++ xyLOGIX.Coinbase.CDP.Tokens.Interfaces/Properties/AssemblyInfo.cs
@@ -0,0 +1,39 @@
+using System.Reflection;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("xyLOGIX.Coinbase.CDP.Tokens.Interfaces")]
+[assembly: AssemblyDescription("The Coinbase Developer Platform gives builders the most trusted, scaled, and reliable onramps and infrastructure available for building onchain apps.  Battle tested.  This module provides objects and functionality to seamlessly generate JSON Web Token(s) (JWTs) for use with requests to the Coinbase (Retail) and Coinbase (Advanced Trade) APIs.  The JWTs are to be generated per each API request and are to be supplied as an 'Authorization Bearer' token.  This module includes functionality to read a user-specified JSON file containing a token name and a private key, or to specify them as arguments to a method.  This particular library holds the C# interfaces for this component.")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("xyLOGIX, LLC")]
+[assembly: AssemblyProduct("xyLOGIX Coinbase REST API Support")]
+[assembly:
+    AssemblyCopyright(
+        "Copyright © 2024 by xyLOGIX, LLC.  All rights reserved.")]
+[assembly:
+    AssemblyTrademark(
+        "This software is a component of xyLOGIX Coinbase REST API Support.  xyLOGIX Coinbase REST API Support is a trademark of xyLOGIX, LLC.  All rights reserved, especially those granted by international treaties.")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("a946c46f-2dd0-415a-9b3a-46cf90f6e414")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
