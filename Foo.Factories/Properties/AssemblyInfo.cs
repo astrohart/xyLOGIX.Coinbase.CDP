9cb04e0b6f7dfca5ab3c81288f64a270140a82e7
Brian Hart <brian.hart@xylogix.net>
2024-05-20 19:52:00 -0600

    Latest updates at 19:52 hours MDT on 05/20/2024

---
+++ Foo.Factories/Properties/AssemblyInfo.cs
@@ -0,0 +1,39 @@
+using System.Reflection;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("Foo.Factories")]
+[assembly: AssemblyDescription("Provides objects and functionality to implement the loading and storing of a Coinbase CDP private key to and from a file on the disk.  This particular library holds the factories for this component.")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("xyLOGIX, LLC")]
+[assembly: AssemblyProduct("xyLOGIX Coinbase REST API Support")]
+[assembly:
+    AssemblyCopyright(
+        "Copyright © 2024 by xyLOGIX, LLC.  All rights reserved.")]
+[assembly:
+    AssemblyTrademark(
+        "This software is a component of xyLOGIX Coinbase REST API Support.  xyLOGIX Coinbase REST API Support is a trademark of xyLOGIX, LLC.  All rights reserved, especially those granted by international treaties.")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("5418170c-f96c-4bf7-be78-8fff57f34cfd")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
