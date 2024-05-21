1b1b7ca874bd0c786a2466fe8434be9add10afc2
Brian Hart <brian.hart@xylogix.net>
2024-05-20 18:05:41 -0600

    Create the 'xyLOGIX.Coinbase.CDP.Tokens.Models' module
    Provides objects and functionality to model the data that typically can be found in .json files distributed by the Coinbase Developer Platform (CDP) upon the generation of a CDP API key.  These files contain the name of the key and the .pem data for the user's private key.
    
    This module is a component of the xyLOGIX Coinbase REST API Support software system.
    
    ---
    Copyright © 2024 by xyLOGIX, LLC.  All rights reserved.

---
+++ xyLOGIX.Coinbase.CDP.Tokens.Models/Properties/AssemblyInfo.cs
@@ -0,0 +1,39 @@
+using System.Reflection;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("xyLOGIX.Coinbase.CDP.Tokens.Models")]
+[assembly: AssemblyDescription("Provides objects and functionality to model the data that typically can be found in .json files distributed by the Coinbase Developer Platform (CDP) upon the generation of a CDP API key.  These files contain the name of the key and the .pem data for the user's private key.  This particular library holds the concrete classes and implementations of the component.")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("xyLOGIX, LLC")]
+[assembly: AssemblyProduct("xyLOGIX Coinbase REST API Support")]
+[assembly:
+    AssemblyCopyright(
+        "Copyright © 2024 by xyLOGIX, LLC.  All rights reserved.")]
+[assembly:
+    AssemblyTrademark(
+        "This software is a component of xyLOGIX Coinbase REST API Support.  xyLOGIX Coinbase REST API Support is a trademark of xyLOGIX, LLC.  All rights reserved, especially those granted by international treaties.")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("2b7d91c1-2a70-4b39-904b-ea5f9a3bfb1b")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
