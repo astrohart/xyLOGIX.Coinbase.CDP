fff215dfea6819e90a3d02c1bb95b700bd00d43c
Brian Hart <brian.hart@xylogix.net>
2024-05-20 18:26:57 -0600

    Update JwtTokenGenerator.cs
    * Created a GenerateFor() method that accepts a reference to an instance of an object that implements the xyLOGIX.Coinbase.CDP.Keys.Models.Interfaces.ICoinbaseJwtPrivateKey interface.
    
    Such an object provides the name and the pem-formatted private key needed in order to create a JSON web token.

--- xyLOGIX.Coinbase.CDP.Tokens/JwtTokenGenerator.cs
+++ xyLOGIX.Coinbase.CDP.Tokens/JwtTokenGenerator.cs
@@ -1,5 +1,8 @@
 ﻿using PostSharp.Patterns.Diagnostics;
+using System;
+using xyLOGIX.Coinbase.CDP.Keys.Models.Interfaces;
 using xyLOGIX.Coinbase.CDP.Tokens.Interfaces;
+using xyLOGIX.Core.Debug;
 
 namespace xyLOGIX.Coinbase.CDP.Tokens
 {
@@ -10,6 +13,25 @@ namespace xyLOGIX.Coinbase.CDP.Tokens
     /// </summary>
     public class JwtTokenGenerator : IJwtTokenGenerator
     {
+        public string GenerateFor(ICoinbaseJwtPrivateKey key)
+        {
+            var result = string.Empty;
+
+            try
+            {
+                // TODO: Add the code that may potentially throw an exception here
+            }
+            catch (Exception ex)
+            {
+                // dump all the exception info to the log
+                DebugUtils.LogException(ex);
+
+                result = string.Empty;
+            }
+            
+            return result;
+        }
+        
         /// <summary>
         /// Empty, static constructor to prohibit direct allocation of this class.
         /// </summary>
